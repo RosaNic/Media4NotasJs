2d782a475e0627deea003b2f0365883707b9a323
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-18 22:37:12 -0300

    adciona exercicio

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("-- Média --\n\nSaiba as suas notas aqui!\n");
+
+let n = parseInt (prompt("Escreva sua primeira nota"));
+console.log("1º Nota" + ": " + n);
+
+let n2 = parseInt (prompt("Escreva sua segunda nota"))
+console.log("2º Nota" + ": " + n2);
+
+let n3 = parseInt (prompt("Escreva sua terceira nota"))
+console.log("3º Nota" + ": " + n3);
+
+let n4 = parseInt (prompt("Escreva sua quarta nota"))
+console.log("4º Nota" + ": " + n4);
+
+let media = parseFloat(((n + n2 + n3 + n4)/2).toFixed(1));
+
+if(n < 0 || n > 10 ||
+     n2 < 0 || n2 > 10 ||
+      n3 < 0 || n3 > 10 ||
+       n4 < 0 || n4 > 10)
+{
+    alert("Valor invalido!\n\nDigite um valor entre 0 e 10");
+} else {
+    alert("Sua média é:" + " " + media);
+}
