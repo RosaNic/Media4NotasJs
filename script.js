504be237c7a0784f65df668ab766a7610c80cfbc
Nicole <rosanik3103@gmail.com>
2024-04-28 00:41:18 -0300

    finaliza

--- script.js
+++ script.js
@@ -18,8 +18,20 @@ if(n < 0 || n > 10 ||
      n2 < 0 || n2 > 10 ||
       n3 < 0 || n3 > 10 ||
        n4 < 0 || n4 > 10)
-{
-    alert("Valor invalido!\n\nDigite um valor entre 0 e 10");
-} else {
-    alert("Sua média é:" + " " + media);
-}
+    {
+        alert("Valor invalido!\n\nDigite um valor entre 0 e 10");
+    } else
+        {   console.log("Sua média é:" + " " + media)    }
+
+if(media < 5)
+    {
+        alert("Você reprovou!\n" + "Sua média foi de: " + media)
+    }
+
+if(media == 6)
+    {
+        alert("Você ficou de recuperação!\n" + "Sua média foi de: " + media)
+    } else
+        {   
+            alert("Você foi aprovado!\n" + "Sua média foi de: " + media)
+        }
